836c338424fdb2a3611c76d17a6a4ce009ece42f
Evgeniy Korin <evgkorin@yandex.ru>
2023-04-23 19:50:08 +0300

    added FilmText

---
+++ src/main/java/ru/javarush/domain/FilmText.java
@@ -0,0 +1,54 @@
+package ru.javarush.domain;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Type;
+
+@Entity
+@Table(schema = "movie", name = "film_text")
+public class FilmText {
+
+    @Id
+    @Column(name = "film_id")
+    private Short id;
+
+    @OneToOne
+    @JoinColumn(name = "film_id")
+    private Film film;
+
+    private String title;
+    @Column(columnDefinition = "text")
+    @Type(type = "text")
+    private String descriprion;
+
+    public Short getId() {
+        return id;
+    }
+
+    public void setId(Short id) {
+        this.id = id;
+    }
+
+    public Film getFilm() {
+        return film;
+    }
+
+    public void setFilm(Film film) {
+        this.film = film;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescriprion() {
+        return descriprion;
+    }
+
+    public void setDescriprion(String descriprion) {
+        this.descriprion = descriprion;
+    }
+}
