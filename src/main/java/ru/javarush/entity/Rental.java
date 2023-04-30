690ed7fc8244189124e07ee86ea6cd90771c06e2
Evgeniy Korin <evgkorin@yandex.ru>
2023-04-30 13:50:18 +0300

    add rental entity

--- src/main/java/ru/javarush/entity/Rental.java
+++ src/main/java/ru/javarush/entity/Rental.java
@@ -1,4 +1,79 @@
 package ru.javarush.entity;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(schema = "movie", name = "rental")
 public class Rental {
+    @Id
+    @Column(name = "rental_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "rental_date")
+    private LocalDateTime rentalDate;
+
+    @ManyToOne
+    @JoinColumn(name = "inventory_id")
+    private Inventory inventory;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @Column(name = "return_date")
+    private LocalDateTime returnDate;
+
+    @ManyToOne
+    @JoinColumn(name = "stuff_id")
+    private Staff staff;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getRentalDate() {
+        return rentalDate;
+    }
+
+    public void setRentalDate(LocalDateTime rentalDate) {
+        this.rentalDate = rentalDate;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public LocalDateTime getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDateTime returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public Staff getStaff() {
+        return staff;
+    }
+
+    public void setStaff(Staff staff) {
+        this.staff = staff;
+    }
 }
